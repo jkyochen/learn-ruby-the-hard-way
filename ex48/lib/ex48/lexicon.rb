4cc32983f2a89ea1f6fbd65825b1f45511dd4812
ChenZiQiang <ziqiang.chen@outlook.com>
2024-03-28 23:59:36 +0900

    Update merge words split

--- ex48/lib/ex48/lexicon.rb
+++ ex48/lib/ex48/lexicon.rb
@@ -2,9 +2,8 @@
 class Lexicon
 
     def Lexicon.scan(sentence)
-        words = sentence.split
         result = []
-        words.each do |word|
+        sentence.split.each do |word|
             if (["north", "south", "east", "west", "down", "up", "left", "right", "back"].include?(word))
                 result.push(["direction", word])
             elsif (["go", "stop", "kill", "eat"].include?(word))
