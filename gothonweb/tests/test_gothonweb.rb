9dd321b14bb98f71b23d57a39f044be1f1da651a
ChenZiQiang <ziqiang.chen@outlook.com>
2024-04-01 10:51:01 +0900

    Add extra test for game by mock http

--- gothonweb/tests/test_gothonweb.rb
+++ gothonweb/tests/test_gothonweb.rb
@@ -11,8 +11,39 @@ class MyAppTest < Test::Unit::TestCase
 
   def test_normal_flow
     get '/'
-    get '/game'
+    follow_redirect!
     assert last_response.body.include?("Central Corridor")
+  end
+
+  def test_error_request_order
+    get '/game'
+    assert last_response.body.include?("You Died!")
+  end
+
+  def test_game_win
+    get '/'
+    post '/game', params={:action => 'tell a joke'}
+    follow_redirect!
+    assert last_response.body.include?("Laser Weapon Armory")
+
+    post '/game', params={:action => '0132'}
+    follow_redirect!
+    assert last_response.body.include?("The Bridge")
+
+    post '/game', params={:action => 'slowly place the bomb'}
+    follow_redirect!
+    assert last_response.body.include?("Escape Pod")
+
+    post '/game', params={:action => '2'}
+    follow_redirect!
+    assert last_response.body.include?("You won!")
+  end
+
+  def test_game_loser
+    get '/'
+    post '/game', params={:action => 'shoot!'}
+    follow_redirect!
+    assert last_response.body.include?("You Died!")
   end
 
 end
